27b3fa02f99bc1797ec2f645c639005f3f242757
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2017-12-11 06:15:05 +0200

    make ajax call for state from within component

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Legend, Tooltip } from 'recharts';
+import reqwest from 'reqwest';
 
-const DataChart = ({ data }) => (
-  <LineChart width={730} height={250} data={data}
-    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-    <CartesianGrid strokeDasharray="3 3" />
-    <XAxis dataKey="date" />
-    <YAxis />
-    <Legend />
-    <Tooltip />
-    <Line type="monotone" dataKey="amount" stroke="#8884d8" />
-  </LineChart>
-);
+class DataChart extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    reqwest({
+      type: 'json',
+      contentType: 'application/json',
+      headers: { accept: 'application/json' },
+      url: '/query',
+      method: 'post',
+      data: { filter: {} },
+    })
+    .then((data) => {
+      this.setState({ data })
+    });
+  }
+
+  render () {
+    return (
+      <LineChart width={730} height={250} data={this.state.data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Legend />
+        <Tooltip />
+        <Line type="monotone" dataKey="amount" stroke="#8884d8" />
+      </LineChart>
+    );
+  }
+}
 
 export default DataChart;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reqwest from 'reqwest';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-reqwest({
-  type: 'json',
-  contentType: 'application/json',
-  headers: { accept: 'application/json' },
-  url: '/query',
-  method: 'post',
-  data: { filter: {} },
-})
-.then((result) => {
-  ReactDOM.render(<App data={result} />, document.getElementById('root'));
-  registerServiceWorker();
-});
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
